ca1c45847986a0d41274cefa23ebfd6448bfbb73
Roman <136304611+rrrkkkvvv@users.noreply.github.com>
2025-04-30 20:27:07 +0200

    modified:   main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const WebSocket = require('ws');
-const tmi = require('tmi.js');
+const WebSocket = require("ws");
+const tmi = require("tmi.js");
 const pkg = require("dotenv");
 
 pkg.config();
@@ -7,50 +7,71 @@ pkg.config();
 const dotenvVars = {
   password: process.env.PASSWORD,
   username: process.env.DB_USER_NAMEHOST,
-   
 };
- 
+
 const wss = new WebSocket.Server({ port: 8080 });
 let sockets = [];
 
-wss.on('connection', ws => {
+wss.on("connection", (ws) => {
   sockets.push(ws);
-  ws.on('close', () => {
-    sockets = sockets.filter(s => s !== ws);
+  ws.on("close", () => {
+    sockets = sockets.filter((s) => s !== ws);
   });
 });
 
 const promoRegex = /^[0-9A-F()+-]+$/;
 const couponRegex = /^[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}$/;
- 
+
 const client = new tmi.Client({
   options: { debug: true },
   identity: {
-    username: dotenvVars.username ,
-    password:  dotenvVars.password,
+    username: dotenvVars.username,
+    password: dotenvVars.password,
   },
-  channels: [  "megarush51", "k0chet", "j05k1y","casebattle_official","joskiyokda" ],
- });
+  channels: [
+    "megarush51",
+    "k0chet",
+    "j05k1y",
+    "casebattle_official",
+    "joskiyokda",
+    "MegaRushPlay",
+  ],
+});
 
 client.connect();
 
 client.on("message", (channel, tags, message, self) => {
   if (self) return;
-  let trimedMessage = message.trim()
-  if((tags.username ==="casebattle_official" ||tags.username === "g1fl_"  ) && couponRegex.test(trimedMessage)){
-    sockets.forEach(ws => ws.send(JSON.stringify({type:"coupon", text: trimedMessage})));
-  }else if ((tags.username === "megarush51" ||tags.username === "joskiyokda" || tags.username === "j05k1y" || tags.username === "g1fl_"  )  && promoRegex.test(trimedMessage)) {
-
-      console.log("Promo found:", trimedMessage);
-      const rebuslessMessage = message.replace(/\(([^()]+)\)/g, (match, expression) => {
+  let trimedMessage = message.trim();
+  if (
+    (tags.username === "casebattle_official" || tags.username === "g1fl_") &&
+    couponRegex.test(trimedMessage)
+  ) {
+    sockets.forEach((ws) =>
+      ws.send(JSON.stringify({ type: "coupon", text: trimedMessage }))
+    );
+  } else if (
+    (tags.username === "megarush51" ||
+      tags.username === "MegaRushPlay" ||
+      tags.username === "joskiyokda" ||
+      tags.username === "j05k1y" ||
+      tags.username === "g1fl_") &&
+    promoRegex.test(trimedMessage)
+  ) {
+    console.log("Promo found:", trimedMessage);
+    const rebuslessMessage = message.replace(
+      /\(([^()]+)\)/g,
+      (match, expression) => {
         try {
-          const result = eval(expression);  
+          const result = eval(expression);
           return result.toString();
         } catch (e) {
-          return match;  
+          return match;
         }
-      });
-      sockets.forEach(ws => ws.send(JSON.stringify({type:"promo", text: rebuslessMessage})));
- 
+      }
+    );
+    sockets.forEach((ws) =>
+      ws.send(JSON.stringify({ type: "promo", text: rebuslessMessage }))
+    );
   }
 });
